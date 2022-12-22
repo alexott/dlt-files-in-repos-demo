523956970b5da776b73fd66a7ac6cc0a3e59440f
Alex Ott <alexott@gmail.com>
2022-12-22 14:56:08 +0100

    use unique names for all DLT pipelines to avoid clashes

--- terraform/azuredevops/dlt.tf
+++ terraform/azuredevops/dlt.tf
@@ -1,17 +1,17 @@
 module "dlt_user" {
   source                  = "./dlt-module/"
-  main_pipeline_name      = "${var.dlt_name_prefix} demo (${data.databricks_current_user.me.alphanumeric})"
+  main_pipeline_name      = "${var.dlt_name_prefix} demo (Dev, ${data.databricks_current_user.me.alphanumeric})"
   main_pipeline_repo_path = databricks_repo.dlt_files_in_repos_in_user_home.path
-  test_pipeline_name      = "${var.dlt_name_prefix} Integration Test (${data.databricks_current_user.me.alphanumeric})"
+  test_pipeline_name      = "${var.dlt_name_prefix} Integration Test (Dev, ${data.databricks_current_user.me.alphanumeric})"
   test_pipeline_repo_path = databricks_repo.dlt_files_in_repos_in_user_home.path
 }
 
 module "dlt_prod" {
   source                       = "./dlt-module/"
   main_pipeline_is_development = false
-  main_pipeline_name           = "${var.dlt_name_prefix} demo (Production)"
+  main_pipeline_name           = "${var.dlt_name_prefix} demo (Production, ${data.databricks_current_user.me.alphanumeric})"
   main_pipeline_repo_path      = databricks_repo.dlt_files_in_repos_in_prod.path
-  test_pipeline_name           = "${var.dlt_name_prefix} Integration Test (Staging)"
+  test_pipeline_name           = "${var.dlt_name_prefix} Integration Test (Staging, ${data.databricks_current_user.me.alphanumeric})"
   test_pipeline_repo_path      = databricks_repo.dlt_files_in_repos_in_staging.path
 }
 
